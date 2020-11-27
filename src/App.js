1b0e8934128ba695bed52e6b7a818eb5c12061b1
Maylo <camilo.iegamar.z@gmail.com>
2020-11-27 13:57:09 -0500

    Despliegue el vercel

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ApiMorty extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      api: []
+    };
+  }
+  componentDidMount() {
+    const peticion = {
+      method: "get",
+      headers: { "Content-Type": "application/json" }
+    };
+    fetch("https://rickandmortyapi.com/api/character?page=1", peticion)
+      .then((res) => res.json())
+      .then((res) => this.setState({ api: res.results }))
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    console.log(this.state.api);
+    return (
+      <div className="body">
+        <h1 className="h1">RICK AND MORTY</h1>
+        <div className="target">
+          {this.state.api.map((api) => {
+            return (
+              <div className="card">
+                <div className="cardHead">
+                  <img className="cardHeadImg" src={api.image} alt={api.name} />
+                  <h3 className="cardHeadName">{api.name}</h3>
+                </div>
+                <div className="cardBody">
+                  <h5 className="cardBodyStatus">Status: {api.status}</h5>
+                  <h5 className="cardBodySpecies">Species: {api.species}</h5>
+                  <h5 className="cardBodyGender">Gender: {api.gender}</h5>
+                  <h5 className="cardBodyOrigin">Origin: {api.origin.name}</h5>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default ApiMorty;
